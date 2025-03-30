8257ecd7fe0b86b8cbb0c2c3d417738c10b3c57d
TzviaIzhakov <tzvia.izhakov@gmail.com>
2025-03-30 23:31:28 +0300

    setup/ first example of detecting images using YOLO

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("first print")
+import os
+os.environ["ULTRALYTICS_NO_CHECKS"] = "1"  # Disable online checks
+import cv2 as cv
+from ultralytics import YOLO
+
+# Load a pretrained YOLOv8 model (e.g., YOLOv8n for a smaller and faster model)
+# model = YOLO('yolov8n.pt') # replace 'n' with 's','m','l', or 'x' for different sizes
+model = YOLO('yolov8x.pt') # replace 'n' with 's','m','l', or 'x' for different sizes
+
+results = model.predict('./images/knife.jpg')
+img = results[0].plot()
+
+cv.imshow('YOLOv8 Object Detection', img)
+cv.waitKey(0)  # Wait for a key press
+cv.destroyAllWindows()  # Close window
